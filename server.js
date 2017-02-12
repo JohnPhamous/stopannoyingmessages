c8de82977d8699ddca23a124118e548b07375f55
aarohmankad <aarohmankad@gmail.com>
2017-02-11 16:22:50 -0800

    error check for null message body

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ login({
     	participant_names = [];
 
  	api.listen(function callback_listen(err, message) {
-		if (err) return console.error(err);
+		if (err || !message.body) return console.error(err);
 
 		var message_body = message.body;
 
