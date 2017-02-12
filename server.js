bb8b45c908f1c30c5a56d5cf8e55c8023a5bc90b
aarohmankad <aarohmankad@gmail.com>
2017-02-11 16:08:05 -0800

    user is notified when they are mentioned

--- server.js
+++ server.js
@@ -14,7 +14,9 @@ login({
 }, function callback (err, api) {
     if (err) return console.error(err);
 
-    var participant_names = [];
+    var
+    	participant_ids = [],
+    	participant_names = [];
 
  	api.listen(function callback_listen(err, message) {
 		if (err) return console.error(err);
@@ -24,6 +26,8 @@ login({
         api.getThreadInfo(message.threadID, function callback_getThreadInfo(err, info) {
         	if (err) return console.error(err);
 
+        	participant_ids = info.participantIDs;
+
         	api.getUserInfo(info.participantIDs, function (err, participant_info) {
         		for (var prop in participant_info) {
         			if (participant_info.hasOwnProperty(prop)) {
@@ -32,9 +36,8 @@ login({
         		}
 
         		for (var id in participant_names) {
-        			
-        			if (message_body.includes(id)) {
-        				api.sendMessage("You've been mentioned!", id);
+        			if (message_body.includes(participant_names[id])) {
+        				api.sendMessage("You've been mentioned!", participant_ids[id]);
         			}
         		}
         	});
