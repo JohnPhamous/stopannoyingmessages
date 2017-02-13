8f1ba5132322618654e1df623497c48d89413cd1
lepatrick714 <lle018@ucr.edu>
2017-02-13 11:33:50 -0800

    Moving typer.py into stable

---
+++ typer.py
@@ -0,0 +1,21 @@
+import pyautogui, time
+
+counter = 10
+counter_2 = 0
+
+name = "Aaroh"
+#phrases = ['Hello {}\n'.format(name), 'Hey {} I like ham\n'.format(name), 'I do not like spam, {}\n'.format(name), 'Spam musabi is nice though, {}\n'.format(name), 'Would you like some {}?\n'.format(name)]
+
+phrases = ['Hey what are we doing today?', 'Aaroh do you want to grab boba?', 'Let\'s go get some food Patrick', 'John did you do the homework', 'Richel want to get some wine?', 'How is everyone\'s day?', 'Did any do #4 on the homework?', 'I saw the most amazing thing today everyone', 'Terry where are you?!?!?!?!?!']
+time.sleep(2)
+i = 0
+while True:
+    pyautogui.typewrite(phrases[i])
+    pyautogui.typewrite('\n')
+    i += 1
+    counter_2 += 1
+    if i == 9:
+        i = 0
+    if counter_2 == 10:
+        break
+
