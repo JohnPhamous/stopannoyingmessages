437eff12bf36c6ff1aead58bb04d23f89bed6c5a
lepatrick714 <lle018@ucr.edu>
2017-02-13 13:27:59 -0800

    Older server.js

--- server.js
+++ server.js
@@ -2,38 +2,57 @@ email = process.argv[2];
 password = process.argv[3];
 
 var
-	Chat = require('./services/Chat'),
+	login = require('facebook-chat-api'),
 	express = require('express'),
 	app = express(),
 	port = process.env.PORT || 8000;
 
-console.log(email, password);
+login({
+    email: email,
+    password: password
+}, function callback(err, api) {
+    if (err) return console.error(err);
 
-Chat
-    .login(email, password)
-    .then(function (api) {
-    	return Chat.listen(api);
-    })
-    .then(function (message) {
-    	var thread_info;
+    var
+        participant_ids = [],
+        participant_names = [];
 
-        return Chat
-    		.getThreadInfo(message)
-    		.then(function (info) {
-                thread_info = info;
-    			return Chat.getUserInfo(info);
-    		})
-    		.then(function (participants_info) {
-    			return Chat.sendMessage(message.body, message.senderID, thread_info, participants_info);
-    		});
-    })
-    .catch(function (err) {
-    	console.log("some error");
-        if (!err) {
-    		return console.log("No message body found");
-    	}
+    api.listen(function callback_listen(err, message) {
+        if (err || !message.body) return console.error(err, message);
 
-    	return console.log(err);
+        if (message.type != "message") {
+        	return;
+        }
+
+        var message_body = message.body.toLowerCase();
+        var message_author = message.senderID;
+
+        api.getThreadInfo(message.threadID, function callback_getThreadInfo(err, info) {
+            if (err) return console.error(err);
+
+            participant_ids = info.participantIDs;
+
+            var thread_name = info.name;
+
+            api.getUserInfo(info.participantIDs, function(err, participant_info) {
+                for (var prop in participant_info) {
+                    if (participant_info.hasOwnProperty(prop)) {
+                        participant_names.push(participant_info[prop].firstName.toLowerCase());
+                    }
+                }
+
+                for (var id in participant_names) {
+                    if (message_body.includes(participant_names[id])) {
+                        api.sendMessage(
+                            "[" + thread_name + "] " +
+                            participant_info[message_author].firstName + ": " +
+                            message_body,
+                            participant_ids[id]);
+                    }
+                }
+            });
+        });
     });
+});
 
 app.listen(port);
